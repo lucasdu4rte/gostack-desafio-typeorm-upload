b87a07a75bb2d9a998238e9e94fbd743aff712f9
Lucas Duarte <lucasdu4rte@gmail.com>
2021-01-28 03:19:32 -0300

    Fix test

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -17,10 +17,11 @@ class TransactionsRepository extends Repository<Transaction> {
       (accumulator, transaction) => {
         switch (transaction.type) {
           case 'income':
-            accumulator.income += transaction.value;
+            accumulator.income += Number(transaction.value);
             break;
+
           case 'outcome':
-            accumulator.income += transaction.value;
+            accumulator.outcome += Number(transaction.value);
             break;
 
           default:
